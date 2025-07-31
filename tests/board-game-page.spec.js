71dde036e3adde2828bbf9a1a73aea5caadb5edd
Kris Lopatko <krzysztof.lopatko@xdesign.com>
2025-07-31 09:34:27 +0100

    Fixed the file name and amended test description.

---
+++ tests/board-game-page.spec.js
@@ -0,0 +1,17 @@
+// @ts-check
+import {
+  test,
+  expect
+} from '@playwright/test';
+
+test('Navigate from the main page to the reviews page', async ({
+  page
+}) => {
+  await page.goto('https://www.board-game.co.uk/');
+
+  // Click the game reviews link.
+  await page.getByAltText('Game Reviews').click();
+
+  // Expect the reviews page to be displayed.
+  await expect(page).toHaveURL('https://www.board-game.co.uk/blog/board-games/reviews/')
+});
